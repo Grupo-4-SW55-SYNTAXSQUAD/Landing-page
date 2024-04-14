30c0a96f4bca636e4ad8ce20d555ca5e64c5e429
Paolo <u202219186@upc.edu.pe>
2024-04-13 19:12:13 -0500

    git commit feature: Añadido About Us
    Se agregó el apartado de About us al código de la landing page, así como
    añadir los estilos del apartado junto a sus imágenes.

---
+++ assents/JavaScript/index.js
@@ -0,0 +1,24 @@
+document.body.classList.add('bg-red')
+document.body.classList.remove('one')
+
+console.log(document.body.classList)
+
+const btnOpen = document.querySelector('.btn--open')
+const btnClose = document.querySelector('.btn--close')
+const navMenu = document.querySelector('.nav__menu')
+console.log(btnOpen)
+
+btnOpen.onclick = function () {
+  navMenu.classList.toggle('show--menu')
+}
+
+btnClose.onclick = function () {
+  navMenu.classList.remove('show--menu')
+}
+const links = document.querySelectorAll('.nav__link')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    navMenu.classList.remove('show--menu')
+  })
+}
